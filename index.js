77461f81e112b26c03baeb41502907b11d569981
Olivier Oeuillot <olivier.oeuillot@gmail.com>
2021-02-17 22:10:33 +0100

    serialport 9.0.6

--- index.js
+++ index.js
@@ -5,3 +5,18 @@ module.exports = {
 	Serial: Tsx17Serial,
 	Protocol: Tsx17Protocol
 };
+
+/*
+const serialport = require('serialport');
+serialport.list().then((ports) => {
+
+	ports.forEach(function (port) {
+		console.log("  Port name='" + port.comName + "' pnpId='" +
+			port.pnpId + "' manufacturer='" + port.manufacturer + "'");
+	});
+	console.log("End of list");
+}, (error) => {
+	console.log("List performs error : " + error);
+	process.exit(0);
+});
+*/
